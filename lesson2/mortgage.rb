1b737fdafec76fbcbfcdd02633ee92ff4192ac6b
Radu Constantin <constantin.raduadrian@yahoo.com>
2019-08-31 17:33:41 +0300

    Finished the mortgage program

--- lesson2/mortgage.rb
+++ lesson2/mortgage.rb
@@ -6,9 +6,15 @@ def valid_num? (number)
   number.to_i > 0 || number.to_f.to_s == number
 end
 
+def valid_period? (number)
+  number.to_i.to_s == number && number.to_i > 0
+end
+
 text("Welcome to the Mortgage calculator!")
 
 text("In order to proceed I will need some information")
+
+loop do #main program loop
 
 loan_amount = ''
 loop do
@@ -32,9 +38,16 @@ text("What is the Anual Percentage Rate (APR)?")
   end
 end
 
-
+dur_year = ''
+loop do
 text("For how many years is the loan?")
   dur_year = gets.chomp
+  if valid_period?(dur_year)
+    break
+  else
+    text("That is not a valid duration.Please try again!")
+end
+end
 
 dur_months = dur_year.to_i * 12
 apr = apr.to_f / 100
@@ -44,3 +57,11 @@ result = loan_amount.to_f * (mpr/(1 - (1 + mpr)**(-dur_months)))
 result = result.round(4)
 
 text("Your monthly payment is #{result}.")
+
+text("Do you want to make another calculation? ('Y'to calculate again)")
+answer = gets.chomp.downcase
+if answer != "y"
+  text ("Thank you for using this program.")
+  break
+end
+end
