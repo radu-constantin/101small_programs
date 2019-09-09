6e3f7597d4b84f6f7d404f50ea8b353528bde136
Radu Constantin <constantin.raduadrian@yahoo.com>
2019-09-09 19:48:13 +0300

    Added bonus functionalities to rock_paper_scissors.

--- lesson2/rock_paper_scissors.rb
+++ lesson2/rock_paper_scissors.rb
@@ -1,9 +1,5 @@
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 
-def test_method
-  prompt('test method')
-end
-
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -26,18 +22,33 @@ def display_result(player, computer)
       prompt("You won!")
     elsif win?(computer, player)
       prompt("Computer won!")
-
-      prompt("Computer won!")
     else
       prompt("It's a tie!")
     end
   end
 
+player_wins = 0
+computer_wins = 0
+
+
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Type - 'r' for rock, 'p' for paper, 's' for scissors, 'l' for lizzard and 'S' for spock.")
     choice = Kernel.gets().chomp()
+
+    case choice
+    when 'r'
+      choice = 'rock'
+    when 'p'
+      choice = 'paper'
+    when 's'
+      choice = 'scissors'
+    when 'l'
+      choice = 'lizzard'
+    when 'S'
+      choice = 'spock'
+    end
 
     if VALID_CHOICES.include?(choice)
       break
@@ -52,9 +63,24 @@ loop do
 
   display_result(choice, computer_choice)
 
+  if win?(choice, computer_choice)
+    player_wins += 1
+  elsif win?(computer_choice, choice)
+    computer_wins += 1
+  end
+
+  if computer_wins == 5
+    prompt "Computer is the grand winner!"
+  elsif player_wins == 5
+    prompt "Player is the grand winner!"
+  end
+
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
+
+
   break unless answer.downcase().start_with?('y')
 end
+
 
 prompt("Thank you for playing!")
