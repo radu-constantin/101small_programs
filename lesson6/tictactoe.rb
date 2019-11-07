bb0553c7e229cf1227ea60b75224eb85db53f365
Radu Constantin <constantin.raduadrian@yahoo.com>
2019-11-07 09:26:15 +0200

    Added Defensive AI. Needs refactoring.

--- lesson6/tictactoe.rb
+++ lesson6/tictactoe.rb
@@ -1,8 +1,9 @@
-
-
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = '0'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                [[2, 5, 8], [1, 4, 7], [3, 6, 9]] +
+                [[1, 5, 9], [3, 5, 7]]
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -18,6 +19,10 @@ def joinor(array, joining_symbol = ", ", link = "or")
   end
 end
 
+score = {
+  'player' => 0,
+  'computer' => 0,
+}
 
 def display_board(brd)
   system 'clear'
@@ -50,16 +55,20 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-  prompt "Choose a square (#{joinor(empty_squares(brd))}):"
-  square = gets.chomp.to_i
-  break if empty_squares(brd).include?(square)
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
 end
   brd[square] = PLAYER_MARKER
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  if detect_danger(brd)
+    square = danger_square(brd)
+  else
+    square = empty_squares(brd).sample
+  end
   brd[square] = COMPUTER_MARKER
 end
 
@@ -72,23 +81,41 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7,8,9]] +
-                  [[2, 5, 8], [1, 4, 7], [3, 6, 9]] +
-                  [[1, 5, 9], [3, 5, 7]]
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-      brd[line[1]] == PLAYER_MARKER &&
-      brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
     elsif
-        brd[line[0]] == COMPUTER_MARKER &&
-        brd[line[1]] == COMPUTER_MARKER &&
-        brd[line[2]] == COMPUTER_MARKER
+        brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
     nil
 end
+
+def detect_danger(brd)
+  danger = false
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2 && brd.values_at(*line).count(INITIAL_MARKER) == 1
+      danger = true
+    else
+      nil
+    end
+  end
+  danger
+end
+
+def danger_square(brd)
+  danger_line = nil
+  danger_square = nil
+  WINNING_LINES.each do |line|
+    danger_line = line  if brd.values_at(*line).count(PLAYER_MARKER) == 2 && brd.values_at(*line).count(INITIAL_MARKER) == 1
+  end
+  danger_line.each {|index| danger_square = index if brd[index] == INITIAL_MARKER}
+  danger_square
+end
+
+
+
 
 loop do
   board = initialize_board
@@ -99,7 +126,6 @@ loop do
 
     player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
-
     computer_places_piece!(board)
     display_board(board)
   break if someone_won?(board) || board_full?(board)
@@ -113,9 +139,26 @@ loop do
     prompt "It's a tie!"
   end
 
-  prompt "Play again? (y or n)"
+  if detect_winner(board) == "Computer"
+    score['computer'] += 1
+  elsif detect_winner(board) == "Player"
+    score['player'] += 1
+  end
+
+  prompt "Computer won #{score['computer']} times."
+  prompt "Player won #{score['player']} times."
+  puts ""
+
+  if score['computer'] == 5
+    prompt "You've been beaten by a dumb computer 5 times, are you sure you want to continue? (y or n)"
+  elsif score['player'] == 5
+    prompt "You've won against a computer with no AI 5 times. Are you sure you want to play again? (y or n)"
+  else
+    prompt "Play again? (y or n)"
+  end
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
+
 
 prompt "Thanks for playing Tic Tac Toe! Goodbye!"
